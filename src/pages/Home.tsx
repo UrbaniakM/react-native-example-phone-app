557e4fdebb39f236f12c97381ac210f51f924d72
Mateusz Urbaniak <31575249+UrbaniakM@users.noreply.github.com>
2020-08-27 13:30:18 +0200

    Create Home and Profile pages

---
+++ src/pages/Home.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+import {ScrollView, Text} from 'react-native';
+
+import {AppBar} from '../components/AppBar';
+
+export const Home: React.FC = () => {
+  return (
+    <>
+      <AppBar title="Home" />
+      <ScrollView>
+        <Text>Home</Text>
+      </ScrollView>
+    </>
+  );
+};
