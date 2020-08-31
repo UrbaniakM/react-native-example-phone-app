424139acbab40cacca93f49aaf1c1a771915c7b1
Mateusz Urbaniak <31575249+UrbaniakM@users.noreply.github.com>
2020-08-31 18:53:32 +0200

    Show info for logged in user on Profile screen

--- src/pages/UserAcceptedNavContainer/Profile/ProfileLoggedIn.tsx
+++ src/pages/UserAcceptedNavContainer/Profile/ProfileLoggedIn.tsx
@@ -3,13 +3,20 @@ import * as React from 'react';
 import {ScrollView, Text} from 'react-native';
 
 import {AppBar} from 'components/AppBar';
+import {useUserNickname} from 'user';
 
 export const ProfileLoggedIn: React.FC = () => {
+  const nickname = useUserNickname();
+
   return (
     <>
       <AppBar title="Profile" />
       <ScrollView>
-        <Text>Profile</Text>
+        {nickname && <Text>Hello, {nickname}! </Text>}
+        <Text>
+          Here, you will be able to preview your preferences and other account
+          details in the future!
+        </Text>
       </ScrollView>
     </>
   );
