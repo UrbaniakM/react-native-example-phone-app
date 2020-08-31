6e488dcf7259e93cf372a5005589358c1cff8b4a
Mateusz Urbaniak <31575249+UrbaniakM@users.noreply.github.com>
2020-08-31 20:53:47 +0200

    Add icons to bottom navigation

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
 
-const STORYBOOK_START = true;
+const STORYBOOK_START = false;
 const appEntry = STORYBOOK_START
   ? require('./.storybook').default
   : require('./src/App').default;
--- src/pages/UserAcceptedNavContainer/UserAcceptedNavContainer.tsx
+++ src/pages/UserAcceptedNavContainer/UserAcceptedNavContainer.tsx
@@ -3,6 +3,8 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {Home} from './Home';
 import {Profile} from './Profile';
 import {StyleSheet, Text} from 'react-native';
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
+import MaterialCommunityIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const Tab = createBottomTabNavigator();
 
@@ -10,35 +12,80 @@ interface TabBarLabelProps {
   focused: boolean;
   color: string;
 }
-const label = 'Long screen name';
-const TabBarLabel: React.FC<TabBarLabelProps> = () => {
-  // const color = focused ? activeTintColor : inactiveTintColor;
-
+const label = 'Product Databases';
+const TabBarLabel: React.FC<TabBarLabelProps> = ({color}) => {
   return (
     <Text
       numberOfLines={2}
-      style={[
-        styles.label,
-        // { color },
-        // horizontal ? styles.labelBeside : styles.labelBeneath,
-        // labelStyle,
-      ]}
+      style={[styles.label, {color}, styles.labelBeside]}
       allowFontScaling>
       {label}
     </Text>
   );
 };
 
-const options = {
-  tabBarLabel: TabBarLabel,
-};
-
 export const UserAcceptedNavContainer: React.FC = () => {
   return (
-    <Tab.Navigator tabBarOptions={{labelStyle: styles.label}}>
-      <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="Long screen name" component={Home} options={options} />
-      <Tab.Screen name="Profile" component={Profile} />
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: ({
+            color,
+            size,
+          }: {
+            focused: boolean;
+            color: string;
+            size: number;
+          }) => <MaterialIcon name="home" color={color} size={size} />,
+        }}
+      />
+      <Tab.Screen
+        name="Information"
+        component={Home}
+        options={{
+          tabBarIcon: ({
+            color,
+            size,
+          }: {
+            focused: boolean;
+            color: string;
+            size: number;
+          }) => <MaterialIcon name="info-outline" color={color} size={size} />,
+        }}
+      />
+      <Tab.Screen
+        name="Product Databases"
+        component={Home}
+        options={{
+          tabBarLabel: TabBarLabel,
+          tabBarIcon: ({
+            color,
+            size,
+          }: {
+            focused: boolean;
+            color: string;
+            size: number;
+          }) => (
+            <MaterialCommunityIcon name="database" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarIcon: ({
+            color,
+            size,
+          }: {
+            focused: boolean;
+            color: string;
+            size: number;
+          }) => <MaterialIcon name="home" color={color} size={size} />,
+        }}
+      />
     </Tab.Navigator>
   );
 };
@@ -47,5 +94,10 @@ const styles = StyleSheet.create({
   label: {
     textAlign: 'center',
     backgroundColor: 'transparent',
+  },
+  // center label with icon
+  labelBeside: {
+    fontSize: 10,
+    marginTop: 3,
   },
 });
