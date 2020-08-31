caa9a6d42b16b087683a2fcea25306aa41000994
Mateusz Urbaniak <31575249+UrbaniakM@users.noreply.github.com>
2020-08-31 19:11:00 +0200

    Hide splash screen in storybook

---
+++ .storybook/index.tsx
@@ -0,0 +1,22 @@
+import {getStorybookUI, configure} from '@storybook/react-native';
+import {loadStories} from './storyLoader';
+import SplashScreen from 'react-native-splash-screen';
+import * as React from 'react';
+
+// React Native packager resolves all imports at build-time, so it’s not possible to load modules dynamically
+// so we're using react-native-storybook-loader package
+configure(loadStories(), module);
+
+const StorybookUI = getStorybookUI({port: 7007, host: 'localhost'});
+
+// RN hot module must be in a class for HMR
+class StorybookUIRoot extends React.Component {
+  componentDidMount() {
+    SplashScreen?.hide?.();
+  }
+  render() {
+    return <StorybookUI />;
+  }
+}
+
+export default StorybookUIRoot;
