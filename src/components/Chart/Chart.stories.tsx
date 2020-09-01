fdddae8a8319027d607e5489f9f74fbeb908b784
Mateusz Urbaniak <31575249+UrbaniakM@users.noreply.github.com>
2020-09-01 16:23:54 +0200

    Create ChartCard story

--- src/components/Chart/Chart.stories.tsx
+++ src/components/Chart/Chart.stories.tsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 import {storiesOf} from '@storybook/react-native';
 import {Chart} from './Chart';
 import {ScrollView} from 'react-native-gesture-handler';
+import {ChartCard} from './ChartCard';
 
 const chartDefault = () => (
   <Chart
@@ -27,6 +28,17 @@ const multipleCharts = () => (
   </ScrollView>
 );
 
+const insideCard = () => (
+  <ChartCard style={{maxHeight: 400, margin: 32}}>
+    <Chart
+      url={
+        'https://public.tableau.com/views/10_0InternationalTourism/InternationalTourism?:embed=y&:tooltip=n&:toolbar=n&:showVizHome=no&:mobile=y&:showAppBanner=n'
+      }
+    />
+  </ChartCard>
+);
+
 storiesOf('Chart', module)
   .add('default', chartDefault)
-  .add('multiple charts', multipleCharts);
+  .add('multiple charts', multipleCharts)
+  .add('inside card', insideCard);
