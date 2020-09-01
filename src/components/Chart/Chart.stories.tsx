99ceb3ed0763d55247243406dc0e83c7011c5b12
Mateusz Urbaniak <31575249+UrbaniakM@users.noreply.github.com>
2020-09-01 13:08:32 +0200

    Add multiple charts Chart story

--- src/components/Chart/Chart.stories.tsx
+++ src/components/Chart/Chart.stories.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import {storiesOf} from '@storybook/react-native';
 import {Chart} from './Chart';
+import {View} from 'react-native';
 
 const chartDefault = () => (
   <Chart
@@ -10,4 +11,26 @@ const chartDefault = () => (
   />
 );
 
-storiesOf('Chart', module).add('default', chartDefault);
+// FIXME: isn't working properly (issue probably related to WebView)
+const multipleCharts = () => (
+  <View>
+    <View>
+      <Chart
+        url={
+          'https://public.tableau.com/views/10_0InternationalTourism/InternationalTourism?:embed=y&:tooltip=n&:toolbar=n&:showVizHome=no&:mobile=y&:showAppBanner=n'
+        }
+      />
+    </View>
+    <View>
+      <Chart
+        url={
+          'https://public.tableau.com/profile/alexander.aj.wray#!/vizhome/MarchMadnessTournamentHistory/Dashboard'
+        }
+      />
+    </View>
+  </View>
+);
+
+storiesOf('Chart', module)
+  .add('default', chartDefault)
+  .add('multiple charts', multipleCharts);
